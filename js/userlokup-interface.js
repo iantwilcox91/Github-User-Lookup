c7c25d3da3176776a86a6f798f2b7204543bf8be
ian Wilcox <ian Wilcox>
2016-10-16 19:29:34 -0700

    update -interface and userlookup

--- js/userlokup-interface.js
+++ js/userlokup-interface.js
@@ -1,7 +1,28 @@
-var apiKey = require('./../.env').apiKey;
+var Repositories = require('./../js/userlookup.js').reposModule;
 
-$(function(){
-  $("#UserNameButton").click(function(){
-    
-  };
+
+
+var displayUser = function(name, userData, reposData) {
+
+  $('.name').append("<h2> The username was " + name + "</h2>");
+
+  $('#allProjectsList').append("<h2><a href=" + reposData + ">Click to see " + name + " repositories.</a>" + "</h2>");
 };
+
+
+
+
+
+$(document).ready(function() {
+  var usersRepos = new Repositories();
+  usersRepos.getRepos();
+
+  $("#UserNameButton").click(function(){
+
+    var name = $('#userName').val();
+    $('#userName').val("");
+    console.log(name);
+    searchRepos.getRepos(name, displayUser);
+
+  });
+});
--- js/userlookup.js
+++ js/userlookup.js
@@ -1,8 +1,18 @@
+var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
+Repositories = function(){
+};
+
+Repositories.prototype.getRepos = function(UserName, displayRepos){
   $.get('https://api.github.com/users/' + UserName + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    displayRepos(UserName, response.login, response.html_url, response.repo_url);
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+
+
+
+exports.reposModule = Repositories;
