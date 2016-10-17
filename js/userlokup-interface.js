3fb00313ba092adec122a97e4c986cd6719e3b6a
ian Wilcox <ian Wilcox>
2016-10-16 19:43:58 -0700

    take out button alert, button works

--- js/userlokup-interface.js
+++ js/userlokup-interface.js
@@ -1,16 +1,11 @@
 var Repositories = require('./../js/userlookup.js').reposModule;
 
-
-
-var displayUser = function(name, userData, reposData) {
+var displayUser = function(name, reposData) {
 
   $('.name').append("<h2> The username was " + name + "</h2>");
 
-  $('#allProjectsList').append("<h2><a href=" + reposData + ">Click to see " + name + " repositories.</a>" + "</h2>");
+  $('#allProjectsList').append("<h2><a href=" + reposData + ">Click to see " + name + "'s repositories.</a>" + "</h2>");
 };
-
-
-
 
 
 $(document).ready(function() {
@@ -18,11 +13,10 @@ $(document).ready(function() {
   usersRepos.getRepos();
 
   $("#UserNameButton").click(function(){
-
     var name = $('#userName').val();
     $('#userName').val("");
     console.log(name);
-    searchRepos.getRepos(name, displayUser);
+    usersRepos.getRepos(name, displayUser);
 
   });
 });
